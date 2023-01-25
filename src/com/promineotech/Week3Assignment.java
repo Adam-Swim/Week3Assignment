3697c89e839d7187e964a764f29f49e445e425e2
Adam Salt <salt48186@gmail.com>
2023-01-25 15:07:51 -0800

    finidhed week3-4 coding assignment

--- src/com/promineotech/Week3Assignment.java
+++ src/com/promineotech/Week3Assignment.java
@@ -42,56 +42,182 @@ public class Week3Assignment {
 		//    * Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console.
 		//    * Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
 		
+		
+		String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+		int sum1 = 0;
+		for (String name : names) {
+			sum1 += name.length();
+		}
+		double average1 = (double) sum1 / names.length;
+		System.out.println(average1);
+		
+		for (String name : names) {
+			System.out.print(name + " ");
+		}
+		
+		
 		//3. How do you access the last element of any array?
 		
+		System.out.println(names[names.length-1]);
+		
+		
 		//4. How do you access the first element of any array?
+		
+		System.out.println(names[1]);
+		
+		
 		
 		//5. Create a new array of int called nameLengths. Write a loop to iterate over the previously created names array and add 
 		//   the length of each name to the nameLengths array.
 		
+		int[] nameLengths = new int [names.length]; 
+		
+		for (int i = 0; i < names.length; i++) {
+			nameLengths[i] = names[i].length();
+		}
+		
+		
+		
 		//6. Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+		int sum2 = 0;
+		for (int j = 0; j < nameLengths.length; j++) {
+			sum2 += nameLengths[j];
+		}
+		System.out.println(sum2);
 		
 		
+		System.out.println(repeatWord("Adam", 7));
+
+		System.out.println(concatfullName("Adam", "Salt"));
+		
+		int[] numbers = {1, 5, 8, 13, 35, 21};
+		System.out.println(isGreaterThan100(numbers));
+		
+		
+		double[] doubles = {12.3, 31.8, 29.9, 67.6, 54.1, 99.0};
+		System.out.println(arrayAverage(doubles));
+		
+		
+		
+		double[] Team1 = {88.6, 67.8, 98.1, 84.5, 87.8};
+		double[] Team2 = { 72.1, 66.7, 89.4, 77.2, 69.4};
+		System.out.println(whichArrayIsAverageIsGreater(Team1, Team2));
+		
+		
+		System.out.println(willBuyDrink(true, 10.50));
+		System.out.println(needsOilChanged(4150, 4));
+		
+	} // end of main
+	
+	
 		//7. Write a method that takes a String, word, and an int, n, as arguments and returns the word concatenated to itself n number of times.
 		//   (i.e. if I pass in “Hello” and 3, I expect the method to return “HelloHelloHello”).
+	
+	  public static String repeatWord(String word, int n) {
+		 String repeatedWord = "";
+		 for (int i = 0; i < n; i++) {
+			 repeatedWord += word;
+		 }
+		 return repeatedWord;
+	 } // end of method
+		
+		
+		
+		
 		
 		//8. Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).
-		
+	public static String concatfullName(String firstName, String lastName) {
+		String fName = firstName + " " + lastName;
+		return fName;
+	}
+	  
+	  
+	  
+	  
+	  
 		//9. Write a method that takes an array of int and returns true if the sum of all the ints in the array is greater than 100.
-		
+	public static boolean isGreaterThan100(int[] nums) {
+		int sum = 0;
+		for (int num : nums) {
+			sum += num;
+		}
+		return sum > 100;
+	}
+	
+	
+	
+	
 		//10. Write a method that takes an array of double and returns the average of all the elements in the array.
 		
+	public static double arrayAverage(double[] averages) {
+		double sum = 0;
+		for (double average : averages) {
+			sum += average;
+		}
+		return sum / averages.length;
+	}
+	
 		//11. Write a method that takes two arrays of double and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 
+	public static boolean whichArrayIsAverageIsGreater(double[] arrayOne, double[] arrayTwo) {
+		double sum1 = 0;
+		double sum2 = 0;
+		for (double array : arrayOne) {
+			sum1 += array;
+		}
+		for (double array : arrayTwo) {
+			sum2 += array;
+		}
+		
+		return sum1 > sum2;
+	}
+	
+	
+	
 		//12. Write a method called willBuyDrink that takes a boolean isHotOutside, and a double moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 		
+	public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
+		if (isHotOutside && moneyInPocket > 10.50) {
+			return true	;
+		} else {
+		return false;
+	}
+	
+	}
 		//13. Create a method of your own that solves a problem. In comments, write what the method does and why you created it.
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	public static boolean needsOilChanged(double miles, int months) {
+		if (miles > 3000 || months > 4) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
-}
+}// end of class
